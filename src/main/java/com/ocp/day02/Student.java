beee03211ce59e9949f0fbf858c324103c7b4d83
student <student@192.168.1.53>
2021-02-19 21:24:23 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day02/Student.java
@@ -0,0 +1,13 @@
+package com.ocp.day02;
+// data class
+public class Student {
+    // 屬性
+    public String schoolName;
+    public String name;
+    public int age;
+    // 方法
+    // 印出學生資料
+    public void print(){
+        System.out.printf("%s %s %s\n", schoolName, name , age);
+    }
+}
