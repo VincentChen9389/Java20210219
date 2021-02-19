b161053492822c6877553a1a4f6e926e8972472f
student <student@192.168.1.53>
2021-02-19 21:44:38 +0800

    Java OCP

--- src/main/java/com/ocp/day02/Student.java
+++ src/main/java/com/ocp/day02/Student.java
@@ -1,11 +1,13 @@
 package com.ocp.day02;
 // data class
+// 物件, 實體, 實例 (instance)(object)
 public class Student {
-    // 屬性
-    public String schoolName;
-    public String name;
+    // 屬性, 變數, 資產, 欄位 (attributes)
+    // 類別的 , 靜態的 (static)
+    public static String schoolName; //類別屬性
+    public String name; // 物件屬性
     public int age;
-    // 方法
+    // 方法 (method)(method is your logical thinking.)
     // 印出學生資料
     public void print(){
         System.out.printf("%s %s %s\n", schoolName, name , age);
