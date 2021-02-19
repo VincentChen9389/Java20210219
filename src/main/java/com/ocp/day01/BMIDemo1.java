69e1a6ecba8e97842658574248213fb635e724de
student <student@192.168.1.53>
2021-02-19 20:09:14 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day01/BMIDemo1.java
@@ -0,0 +1,25 @@
+package com.ocp.day01;
+
+public class BMIDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello 哈囉");
+        // 計算 BMI
+        String name = "John";
+        double height = 170; //170.0
+        double weight = 60;
+        double result = weight / Math.pow(height/100, 2);
+        
+        System.out.printf("%s 的身高: %.1f 體重: %.1f bmi: %.2f\n",
+                name, height, weight, result);
+        
+        String name2 = "Mary";
+        double height2 = 160;
+        double weight2 = 48;
+        double result2 = weight2 / Math.pow(height2/100, 2);
+        
+        System.out.printf("%s 的身高: %.1f 體重: %.1f bmi: %.2f\n",
+                name2, height2, weight2, result2);
+    }
+}
+// Shift + F6 --> run file
+// f2 --> refactor
